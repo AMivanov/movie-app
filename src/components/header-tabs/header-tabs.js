cf7170e0f7cf5274515efac729a9c6daa278dbda
amivanov <ivavov.am@list.ru>
2024-03-15 22:21:15 +0300

    v6 2.3.17

--- src/components/header-tabs/header-tabs.js
+++ src/components/header-tabs/header-tabs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs } from 'antd';
+import { Spin, Tabs } from 'antd';
 
 import './header-tabs.css'
 import { GetRating } from '../../services/rating-service';
@@ -8,18 +8,23 @@ import MainContainer from '../main-container';
 export default class HeaderTabs extends React.Component {
     state = {
         ratedFilms: [],
+        loading: false,
         currentPageRated: 1,
         totalRatedPages: 0,
     }
 
     onChangeTabs = (key) => {
         if (key === '2') {
+            this.setState({
+                loading: true,
+            })
             GetRating(this.props.guestSessionId, this.state.currentPageRated)
                 .then((res) => {
                     if (res.success === false) {
                         throw new Error('Server returned false')
                     }
                     this.setState({
+                        loading: false,
                         ratedFilms: res.results,
                         totalRatedPages: res.total_pages,
                     })
@@ -55,9 +60,11 @@ export default class HeaderTabs extends React.Component {
     }
 
     render() {
-        const { guestSessionId, film, loading,
+        const {
+            guestSessionId, film, loading,
             currentPage, totalPages, searchTerm,
-            searchFilms, handleSearch, handlePageChange } = this.props
+            searchFilms, handleSearch, handlePageChange,
+        } = this.props
         const optionsList = [
             {
                 key: '1',
@@ -82,30 +89,34 @@ export default class HeaderTabs extends React.Component {
                 key: '2',
                 label: 'Rated',
                 children: (
-                    <MainContainer
-                      tabKey="2"
-                      guestSessionId={guestSessionId}
-                      ratedFilms={this.state.ratedFilms}
-                      film={film}
-                      loading={loading}
-                      currentPage={this.state.currentPageRated}
-                      totalPages={this.state.totalRatedPages}
-                      searchTerm={searchTerm}
-                      searchFilms={searchFilms}
-                      handleSearch={handleSearch}
-                      handlePageChange={this.handlePageChangeRated}
-                    />
+                    this.state.loading ? (
+                        <Spin />
+                    ) : (
+                        <MainContainer
+                          tabKey="2"
+                          guestSessionId={guestSessionId}
+                          ratedFilms={this.state.ratedFilms}
+                          film={film}
+                          loading={loading}
+                          currentPage={this.state.currentPageRated}
+                          totalPages={this.state.totalRatedPages}
+                          searchTerm={searchTerm}
+                          searchFilms={searchFilms}
+                          handleSearch={handleSearch}
+                          handlePageChange={this.handlePageChangeRated}
+                        />
+                    )
                 ),
             },
         ];
         return (
-                <Tabs
-                  defaultActiveKey="1"
-                  items={optionsList}
-                  onChange={this.onChangeTabs}
-                  tabBarStyle={{ fontFamily: 'Inter, sans-serif', margin: '0 auto 16px' }}
-                  tabBarGutter={16}
-                />
+            <Tabs
+              defaultActiveKey="1"
+              items={optionsList}
+              onChange={this.onChangeTabs}
+              tabBarStyle={{ fontFamily: 'Inter, sans-serif', margin: '0 auto 16px' }}
+              tabBarGutter={16}
+            />
         )
     }
 }
